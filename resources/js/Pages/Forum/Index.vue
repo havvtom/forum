6c593e842be1138512d00f4a37c81eadc67694f0
= <=>
2024-12-22 17:12:18 +0200

    layouts

--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -1,24 +1,31 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import ForumLayout from '@/Layouts/ForumLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Forum" />
 
-    <AuthenticatedLayout>
+    <ForumLayout>  
         
-
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
-                    <div class="p-6 text-gray-900">
-                        
-                    </div>
-                </div>
+        <div
+            class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+        >
+            <div class="p-6 text-gray-900">
+                
             </div>
         </div>
-    </AuthenticatedLayout>
+        
+
+        <template #side >
+            <div
+                class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+            >
+                <div class="p-6 text-gray-900">
+                    Side
+                </div>
+            </div>
+        </template>
+        
+    </ForumLayout>
 </template>
