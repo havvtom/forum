75f594fe9aa97a07ba0d615d438247f89b718e91
= <=>
2024-12-22 17:48:06 +0200

    creating and listing topics

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Resources\TopicResource;
+use App\Models\Topic;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -31,6 +33,7 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
+            'topics' => TopicResource::collection(Topic::orderBy('name', 'asc')->get()),
             'auth' => [
                 'user' => $request->user(),
             ],
--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -1,6 +1,9 @@
 <script setup>
+import InputLabel from '@/Components/InputLabel.vue';
+import Select from '@/Components/Select.vue';
 import ForumLayout from '@/Layouts/ForumLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
 </script>
 
 <template>
@@ -12,7 +15,14 @@ import { Head } from '@inertiajs/vue3';
             class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
         >
             <div class="p-6 text-gray-900">
-                
+                <div>
+                    <InputLabel for="topic" value="Topic" class="sr-only"/>
+                    <Select>
+                        <option value="" >All topics</option>
+                        <option v-for="topic in $page.props.topics" :key="topic.id" :value="topic.slug" >
+                            {{topic.name}}</option>
+                    </Select>
+                </div>
             </div>
         </div>
         
